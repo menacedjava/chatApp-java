11de3178033e48f6dc5a06726b13b178597a3b83
Madumarov Davron <menaceteacher@gmail.com>
2025-04-14 03:04:40 +0500

    add

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,15 +1,32 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private Status status;
 
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public Account(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
 }
