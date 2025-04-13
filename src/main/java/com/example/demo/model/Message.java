19c599b5cc45f2bbea055b25f35a1facc22c10e7
Madumarov Davron <menaceteacher@gmail.com>
2025-04-14 03:07:48 +0500

    add

---
+++ src/main/java/com/example/demo/model/Message.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String chatId;
+    private String senderId;
+    private String receiverId;
+    private String content;
+    private Date timestamp;
+}
